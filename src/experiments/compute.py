0edc5f7f2e32b3fbf92b3a341c0a5ae31f8d739b
Tuhin Chakrabarty <tuhinjubcse@gmail.com>
2021-01-08 03:33:02 -0500

    Add files via upload

--- src/experiments/compute.py
+++ src/experiments/compute.py
@@ -10,24 +10,21 @@ os.environ["CUDA_VISIBLE_DEVICES"]='0'
 count = 0
 c = 0
 d = 0
-m = {'not_entailment' : [0,1],'entailment': [2]}
+m = {0: 'not_entailment' , 1: 'not_entailment', 2:'entailment'}
 for line in open('simile-entail.txt'):
         line = ast.literal_eval(line) #json.loads(line.strip())
         premise = line["premise"]
         hypothesis = line["hypothesis"]
         count = count +1
-        label = m[line["label"]]
         tokens = roberta.encode(premise,hypothesis)
         tokens1 = roberta.encode(hypothesis)
         val1 = roberta.predict('mnli', tokens).argmax().tolist()
         val2 = roberta.predict('mnli', tokens1).argmax().tolist()
-        if val1 in m['not_entailment'] and line["label"]=="not_entailment":
+        if m[val1]==line["label"]:
                 c = c+1
-        if val1 in m['entailment'] and line["label"]=="entailment":
-                c = c+1
-        if val2 in m['not_entailment'] and line["label"]=="not_entailment":
-                d = d+1
-        if val2 in m['entailment'] and line["label"]=="entailment":
+        else:
+                print(line)
+        if m[val2]==line["label"]:
                 d = d+1
 
 print("Normal",float(c)/float(count))
